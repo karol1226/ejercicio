003ccd432e6ae6353bafb85108536b8dc7079ab4
karol1226 <130701587+karol1226@users.noreply.github.com>
2023-04-17 10:40:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var nombre="mariam";
+var edad= 19;
+var desarrollador=true
+console.log("nombre: "+ nombre)
+console.log("edad: "+ edad)
+console.log("¿eres desarrolador? "+ desarrollador)
+
+const año = new Date();
+console.log(año);
+const fecha = new Date("december 2003");
+console.log(fecha);
+const libro={
+titulo:"el secreto",
+autor:" rhonda byrne",
+fecha:"17/07/2023",
+URL:"https://www.casadellibro.com.co/libro-el-secreto/9788479536442/1136037"
+}
+
+console.log(libro)
